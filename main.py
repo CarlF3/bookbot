89b5bb4d8b0ae8e45fb8fa2d385229e176f88ee0
CarlF3 <156613411+CarlF3@users.noreply.github.com>
2024-10-27 22:17:59 +0000

    Improved readability

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
-    with open("books/frankenstein.txt", "r") as book:
-        print(count_words(book.read()))
-        book.close()
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(count_words(text))
 
 def count_words(book: str):
     word_array = book.split()
     return len(word_array)
+
+def get_book_text(path: str):
+    with open(path) as book:
+        return book.read()
 
 
 main()
