fe3475c4e770dbe2b47830d852eed740afeaa65b
CarlF3 <156613411+CarlF3@users.noreply.github.com>
2024-10-27 22:26:57 +0000

    Added count characters method

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(count_words(text))
+    print(count_characters(text))
 
-def count_words(book: str):
-    word_array = book.split()
+def count_words(text: str):
+    word_array = text.split()
     return len(word_array)
 
 def get_book_text(path: str):
     with open(path) as book:
         return book.read()
-
+    
+def count_characters(text: str):
+    chars = {}
+    for char in text.lower():
+        if char in chars.keys():
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
 
 main()
